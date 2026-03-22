ad6ab23fffdf116566c77a97c081a7d975f68284
blourvim <blourvim@gmail.com>
2026-03-22 18:31:57 +0300

    implement scrolling

--- content/content.js
+++ content/content.js
@@ -17,12 +17,27 @@ document.addEventListener("keydown", (e) => {
     return;
   }
 
-  if (mode === "normal") {
+  if (mode == "normal") {
     e.preventDefault();
     switch (e.key) {
+      // mode switches
       case "i": setMode("insert"); break;
       case ":": setMode("command"); break;
       case "h": setMode("hint"); break;
+
+      // function keys :
+      //
+      // ing page navigations
+      case "j": scrollBy({ top: 100 }); break;
+      case "k": scrollBy({ top: -100 }); break;
+      case "g": break;
+      case "gg": break; //todo: this will need an input buffer system
+      case "G": break;
+
+      case "d": break;
+      case "H": break;
+      case "b": break;
+      case "o": break;
     }
 
   }
